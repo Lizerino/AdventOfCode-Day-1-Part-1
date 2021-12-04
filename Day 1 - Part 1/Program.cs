452726ac7d53e42c5c7908f1615f3a1a1b4e4f25
Stefan Karlsson <stefan.lg.karlsson@gmail.com>
2021-12-04 21:12:55 +0100

    Add project files.

---
+++ Day 1 - Part 1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Get depths
+string[] depths = System.IO.File.ReadAllLines(@"Input.txt");
+
+// Init counter
+int deeper = 0;
+
+// Compare each depth with previous depth
+// if deeper thatn first depth add 1 to counter else continue
+for (int i = 1; i < depths.Length; i++)
+{
+    if (int.Parse(depths[i])> int.Parse(depths[i-1]))
+    {
+        deeper++;
+    }
+}
+
+// print result
+Console.WriteLine(deeper);
